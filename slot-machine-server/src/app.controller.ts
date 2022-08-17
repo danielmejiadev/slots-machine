120207c8ba0478044e3cade553c8950cb3ceece9
Daniel Mejia <danielmejia@Daniels-MacBook-Pro-2.local>
2022-08-17 15:35:09 -0500

    Create the endpoints backbone description and documentation for each one

--- slot-machine-server/src/app.controller.ts
+++ slot-machine-server/src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { AppService, GameType, UserAccount } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/game')
+  @ApiOkResponse({ description: 'The game was initiated successfully' })
+  getGame(): GameType {
+    return this.appService.getGame();
+  }
+
+  @Post('/play')
+  @ApiOkResponse({ description: 'The user play successfully' })
+  play(): GameType {
+    return this.appService.play();
+  }
+
+  @Get('/cash-out')
+  @ApiOkResponse({ description: 'The user cash-out rewards to the account.' })
+  castOut(): UserAccount {
+    return this.appService.cashOut();
   }
 }
--- slot-machine-server/src/app.service.ts
+++ slot-machine-server/src/app.service.ts
@@ -1,8 +1,85 @@
 import { Injectable } from '@nestjs/common';
 
+const getRandomResult = () => {
+  return 0;
+};
+
+const slightlyCheat = () => {
+  return 0;
+};
+
+const cheat = () => {
+  return 0;
+};
+
+const game = {
+  houseCheats: 0,
+  credits: 10,
+  cost: 1,
+  result: [null, null, null],
+  symbols: [
+    {
+      name: 'cherry',
+      icon: '🍒',
+      reward: 10,
+    },
+    {
+      name: 'lemon',
+      icon: '🍋',
+      reward: 20,
+    },
+    {
+      name: 'orange',
+      icon: '🟠',
+      reward: 30,
+    },
+    {
+      name: 'watermelon',
+      icon: '🍉',
+      reward: 40,
+    },
+  ],
+};
+
+let userAccount = {
+  name: '',
+  credits: 0,
+};
+
+export type UserAccount = typeof userAccount;
+export type GameType = typeof game;
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getGame(): GameType {
+    return game;
+  }
+
+  play(): GameType {
+    let result;
+    if (game.credits < 40) {
+      result = getRandomResult();
+    } else if (game.credits >= 40 && game.credits < 60) {
+      game.houseCheats += 1;
+      result = slightlyCheat();
+    } else {
+      game.houseCheats += 1;
+      result = cheat();
+    }
+
+    return {
+      ...game,
+      result,
+      credits: game.credits - game.cost,
+    };
+  }
+
+  cashOut(): UserAccount {
+    userAccount = {
+      ...userAccount,
+      credits: game.credits,
+    };
+
+    return userAccount;
   }
 }
